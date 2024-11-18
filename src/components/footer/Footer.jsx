6fec49980f085173e12e043148273dc5546b8411
Lucasmoraes7 <lm9602837@gmail.com>
2024-11-18 19:17:10 -0300

    footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; 2024 HortiFruti Store</p>
+    </footer>
+
+  )
+}
+
+export default Footer
